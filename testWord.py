90f4b008d157642601207dfed4a71c83bd7bac6f
qiweizhen <zkdqwz@mail.ustc.edu.cn>
2018-10-28 16:00:09 +0800

    step-1

--- testWord.py
+++ testWord.py
@@ -1,8 +1,8 @@
 import unittest
 
-from alphabet import alphabet
+from word import fileWordCounter, alphabet
 
-class TestAlphabet(unittest.TestCase):
+class TestWord(unittest.TestCase):
     
     def setUp(self):
         return super().setUp()
@@ -10,7 +10,7 @@ class TestAlphabet(unittest.TestCase):
     def tearDown(self):
         return super().tearDown()
 
-    def test_init(self):
+    def test_alphabet_init(self):
         filepath = "./test/alphabet.txt"
         count = alphabet(filepath)
         self.assertEqual(count[0], ('a', 0.50))
@@ -18,15 +18,27 @@ class TestAlphabet(unittest.TestCase):
         self.assertEqual(count[2], ('c', 0.20))
         self.assertEqual(count[3], ('d', 0.05))
 
-    def test_nofile(self):
+    def test_alphabet_nofile(self):
         filepath = "./wrong.txt"
         with self.assertRaises(FileNotFoundError):
             count = alphabet(filepath)
 
-    def test_noalphabet(self):
+    def test_alphabet_noalphabet(self):
         filepath = "./test/number.txt"
         count = alphabet(filepath)
         self.assertEqual(len(count), 0)
 
+    def test_word_init(self):
+        filepath = "./test/word.txt"
+        count = fileWordCounter(filepath, 10)
+        result = [('word', 100), ('hello', 70), ('a', 60), ('boy', 60), ('key', 50)]
+        self.assertEqual(count, result)
+
+    def test_word_noword(self):
+        filepath = "./test/noword.txt"
+        count = fileWordCounter(filepath, -1)
+        self.assertEqual(len(count), 0)
+
 if __name__ == "__main__":
     unittest.main()
+
